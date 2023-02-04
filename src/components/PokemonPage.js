6a74e82070241661ad3b48514524caf2f6e05e50
Ayah Elsais <elsais.ayah@gmail.com>
2023-02-04 14:09:17 -0500

    imported useState added pokemon data

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+const [pokemonData, setPokemonData] = useState([]);
+
   useEffect(() => {
     fetch ("http://localhost:3001/pokemon")
     .then(response => response.json())
