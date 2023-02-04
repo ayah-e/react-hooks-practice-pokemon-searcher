f5b048ef1b4271006e3eb2e9217d1c7e54d821ff
Ayah Elsais <elsais.ayah@gmail.com>
2023-02-04 14:55:49 -0500

    added pokemonCard component to collection

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,11 +2,17 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection({pokemonData}) {
+function PokemonCollection({ pokemonData }) {
   const pokemonCards = pokemonData.map((pokemon) => {
     return (
-    <PokemonCard key = {pokemon.id} name = {pokemon.name} sprite = {pokemon.sprites} hp = {pokemon.hp}/>
-  )})
+      <PokemonCard
+        key={pokemon.id}
+        name={pokemon.name}
+        sprite={pokemon.sprites}
+        hp={pokemon.hp}
+      />
+    );
+  });
 
   return (
     <Card.Group itemsPerRow={6}>
